f182b1330e7b7f3aa5de3c70badd3eb72fc66709
KiprotichKuresoi <korirpaul38@gmail.com>
2023-08-10 07:58:11 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ * main -it prints out a string to  stdout
+ *
+ * Return: always (0) if success.
+ * 
+ */
+int main(void)
+
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
