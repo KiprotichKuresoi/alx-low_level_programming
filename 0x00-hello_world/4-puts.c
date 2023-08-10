906f12b4734aaf40d6392f910304ace7272e115c
KiprotichKuresoi <korirpaul38@gmail.com>
2023-08-10 09:37:12 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /** 
  * main -it prints "Programming is like building a multilingual puzzle
- *puzzle, followed by newline."
+ * puzzle, followed by newline."
  * Return: always (0)
  *
  */
 int main(void)
 
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 }
