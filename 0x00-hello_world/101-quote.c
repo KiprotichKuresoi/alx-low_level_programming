a11e54d7cdf7f0fffc7a956b01c33dcacd6540e0
KiprotichKuresoi <korirpaul38@gmail.com>
2023-08-10 14:36:38 -0700

    7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
-/**Main - prints "and that  piece of art is useful". 
- * followed by snewline and standard output.
+/**
+ * Main - prints "and that  piece of art is useful".
+ * Followed by snewline and standard output.
  * Return: always 1 if success.
  */
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora kopar, 2015-10-19\n", 59);
 	return (1);
 }
